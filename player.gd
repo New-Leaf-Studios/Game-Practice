11ec5a4897fa1cf0275879e9d1b877700194d971
Trackstar <139496309+PopRocks655@users.noreply.github.com>
2026-06-12 19:30:56 -0400

    checking multi compat
    commented out movent script

--- player.gd
+++ player.gd
@@ -4,6 +4,6 @@ var direction: Vector2 = Vector2(1,1)
 var speed: int = 5
 
 func _physics_process(_delta: float) -> void:
-	direction = Input.get_vector("left", "right", "up", "down")
+	#direction = Input.get_vector("left", "right", "up", "down")
 	
 	position += direction * speed
