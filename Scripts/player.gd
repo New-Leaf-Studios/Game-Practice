f49c7f4ce18f07088769d56d5d12a2b2b1d6c8bd
Trackstar <139496309+PopRocks655@users.noreply.github.com>
2026-06-12 22:09:29 -0400

    added base animations to player
    TT now has down facing anims

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,6 +13,10 @@ func _physics_process(_delta: float) -> void:
 	else:
 		position += direction * speed
 	
+	if abs(direction.y) > abs(direction.x):
+		$player.play("down")
+	else:
+		$player.play("side")
 	
 	if direction.x >= 0.1:
 		$player.flip_h = true
