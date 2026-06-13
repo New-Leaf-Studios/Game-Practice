6e65d55793106121ac1905dae55c612caed5e4b9
Trackstar <139496309+PopRocks655@users.noreply.github.com>
2026-06-12 21:08:14 -0400

    added sprinting
    Input mapped to "dash" with R or shift key

--- player.gd
+++ player.gd
@@ -2,17 +2,20 @@ extends Node2D
 
 
 var direction: Vector2 = Vector2(1,1)
-var speed: int = 2
+var speed: int = 3
+var sprintSpeed: float = 5.5
 
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	
-	position += direction * speed
+	if Input.is_action_pressed("dash"):
+		position += direction * sprintSpeed
+	else:
+		position += direction * speed
+	
+	
 	if direction.x >= 0.1:
 		$player.flip_h = true
 	elif direction.x <= -0.1:
 		$player.flip_h = false
 	
-	print(direction)
-#up = 0,-1
-#down = 0,1
