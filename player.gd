3b1e05db1b4349baff93cc8600bceaad99fc3a95
XMMC1229 <micahc0608@gmail.com>
2026-06-12 20:57:32 -0400

    Testing timothy movement
    he finally moves correctly

--- player.gd
+++ player.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
+
 var direction: Vector2 = Vector2(1,1)
-var speed: int = 9
+var speed: int = 2
 
 func _physics_process(_delta: float) -> void:
-	#direction = Input.get_vector("left", "right", "up", "down")
+	direction = Input.get_vector("left", "right", "up", "down")
 	
 	position += direction * speed
+	if direction.x >= 0.1:
+		$player.flip_h = true
+	elif direction.x <= -0.1:
+		$player.flip_h = false
+	
+	print(direction)
+#up = 0,-1
+#down = 0,1
