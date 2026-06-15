7e454a8712640fa2a9b711748ed5cedecc2ae3d2
Trackstar <139496309+PopRocks655@users.noreply.github.com>
2026-06-14 23:19:42 -0400

    added turn functionality
    currently it just passes the enemy turn after 1.5 seconds but thats alright

--- Scripts/battlefield.gd
+++ Scripts/battlefield.gd
@@ -1,16 +1,18 @@
 extends TileMapLayer
 
-var player_scene: PackedScene = preload("res://Scenes/combat_player.tscn")
+var entity_scene: PackedScene = preload("res://Scenes/combat_player.tscn")
+var player = entity_scene.instantiate() as Node2D
 
-var player = player_scene.instantiate() as Node2D
 var current_position: Vector2i
 
+enum TurnState {PLAYER_TURN, ENEMY_TURN, PROCESSING}
+var current_turn = TurnState.PLAYER_TURN
 
 func _ready() -> void:
 	$Fighters.add_child(player)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("select"):
+	if event.is_action_pressed("select") && current_turn == TurnState.PLAYER_TURN:
 		
 		var mouse_position: Vector2 = get_local_mouse_position()
 		var grid_position: Vector2i = local_to_map(mouse_position)
@@ -18,6 +20,20 @@ func _unhandled_input(event: InputEvent) -> void:
 		if abs(current_position.x - grid_position.x) + abs(current_position.y - grid_position.y) <= 5:
 			player.position = map_to_local(grid_position)
 			current_position = grid_position
+			current_turn = TurnState.ENEMY_TURN
 		else:
 			print("too far!")
-		
+			
+func _process(delta: float) -> void:
+	match current_turn:
+		TurnState.PLAYER_TURN:
+			pass
+		TurnState.ENEMY_TURN:
+			print("pretend enemy moved here")
+			current_turn = TurnState.PROCESSING
+			print("processing")
+			await get_tree().create_timer(1.5).timeout
+			print("ok its your turn now.")
+			current_turn = TurnState.PLAYER_TURN
+		TurnState.PROCESSING:
+			pass
