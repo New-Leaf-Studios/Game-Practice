27c1bf0915125b0435bae72999fa0e2826b809e3
Trackstar <139496309+PopRocks655@users.noreply.github.com>
2026-06-14 22:43:28 -0400

    Limit movement
    5 tile temporary testing limit using manhattan distance

--- Scripts/battlefield.gd
+++ Scripts/battlefield.gd
@@ -3,6 +3,8 @@ extends TileMapLayer
 var player_scene: PackedScene = preload("res://Scenes/combat_player.tscn")
 
 var player = player_scene.instantiate() as Node2D
+var current_position: Vector2i
+
 
 func _ready() -> void:
 	$Fighters.add_child(player)
@@ -13,7 +15,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		var mouse_position: Vector2 = get_local_mouse_position()
 		var grid_position: Vector2i = local_to_map(mouse_position)
 		
+		if abs(current_position.x - grid_position.x) + abs(current_position.y - grid_position.y) <= 5:
+			player.position = map_to_local(grid_position)
+			current_position = grid_position
+		else:
+			print("too far!")
 		
-		player.position = map_to_local(grid_position)
-		
-		print ("you clicked pixel: ", mouse_position, " which is tile: ", grid_position)
