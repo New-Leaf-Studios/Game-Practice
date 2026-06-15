ba0cba7e13b00bd41e89596d0d518f1251e2ca49
Trackstar <139496309+PopRocks655@users.noreply.github.com>
2026-06-14 22:16:31 -0400

    added player battle move func
    just basics for now

--- Scripts/battlefield.gd
+++ Scripts/battlefield.gd
@@ -1,9 +1,19 @@
 extends TileMapLayer
 
+var player_scene: PackedScene = preload("res://Scenes/combat_player.tscn")
+
+var player = player_scene.instantiate() as Node2D
+
+func _ready() -> void:
+	$Fighters.add_child(player)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("select"):
 		
 		var mouse_position: Vector2 = get_local_mouse_position()
 		var grid_position: Vector2i = local_to_map(mouse_position)
+		
+		
+		player.position = map_to_local(grid_position)
 		
 		print ("you clicked pixel: ", mouse_position, " which is tile: ", grid_position)
